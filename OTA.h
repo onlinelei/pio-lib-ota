97a557e87eadc4a992a354d5c6970e7fece9b0ad
钱磊 <onlinelei@sina.cn>
2024-03-10 14:46:50 +0800

    ota lib

---
+++ OTA.h
@@ -0,0 +1,14 @@
+// Created by Ray on 2023/8/30.
+
+#ifndef AUTOOAT_OTAUTIL_H
+#define AUTOOAT_OTAUTIL_H
+
+
+class OTAUtil {
+public:
+    static void OTAInitLocalNet();
+    static void OTAHandler();
+};
+
+
+#endif //AUTOOAT_OTAUTIL_H
